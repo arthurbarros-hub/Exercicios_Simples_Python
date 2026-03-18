e69d178ab4eda5fad67066a1c9196e9216eef97f
Athur Mansur <barrosarthur312@gmail.com>
2026-03-18 17:39:50 -0300

    Exercicios simples de python para estudo.

---
+++ EntradaSaida1/Ex7.py
@@ -0,0 +1,6 @@
+celsius = input("Informe a temperatura atual em graus celsius: ")
+
+valor = float(celsius)
+
+print(f'A temperatura atual em Fahrenheit é de: {1.8 * valor + 32: .2f}')
+print(f'A temperatura atual em Kelvin é de: {valor + 273}')
